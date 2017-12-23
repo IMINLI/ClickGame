fe91782995c833316a77153e8ff36375d0adb9a3
4419-1-6\user <asd14104220330618@gmail.com>
2017-12-23 11:47:11 +0800

    1.finish healthcomponent

--- Assets/Scripts/View/Behavier/EnemyBehavier.cs
+++ Assets/Scripts/View/Behavier/EnemyBehavier.cs
@@ -2,21 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 [RequireComponent(typeof(Animator))]//attribut //強制掛Animator
+[RequireComponent(typeof(MeshFader))]
 public class EnemyBehavier : MonoBehaviour {
 
     private Animator animator;//指標
+    private MeshFader meshFader;
+    private AudioSource audioSource;
+    private HealthComponent healthComponent;
+    [SerializeField]
+    private AudioClip hurtClip;
     private void Awake()
     {
         animator = GetComponent<Animator>();//設定指標
         //animator.SetTrigger("die"); 
+        meshFader = GetComponent<MeshFader>();
+        audioSource = GetComponent<AudioSource>();
+        healthComponent = GetComponent<HealthComponent>();
     }
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void DoDamage(int attack)
+    {
+        animator.SetTrigger("hurt");
+        audioSource.clip = hurtClip;
+        audioSource.Play();
+        healthComponent.Hurt(attack);
+    }
+    private void Update()
+    {
+        if (healthComponent.IsOver)
+            return;
+        if(Input.GetButtonDown("Fire1"))//滑鼠左鍵
+        {
+            DoDamage(10);//扣10滴血
+        }
+    }
 }
---
+++ Assets/Scripts/View/Component/HealthComponent.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthComponent : MonoBehaviour {
+
+    [SerializeField]//序列化欄位 
+    private Slider healthSlider;
+    protected int currentHealth;
+    [SerializeField]
+    protected float speed = 5f;
+
+
+    public bool IsOver
+    {
+        get
+        {
+            return currentHealth <= healthSlider.minValue;
+        }
+    }
+
+    [ContextMenu("Test Init 100")]
+    private void TestInit()
+    {
+        Init(100);
+    }
+    [ContextMenu("Test Hurt 50")]
+    private void TestHurt()
+    {
+        Hurt(50);
+    }
+
+    public void Init(int maxHealth)
+    {
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = maxHealth;
+        currentHealth = maxHealth;
+    }
+
+    public void Hurt(int damage)
+    {
+        currentHealth -= damage;
+        currentHealth = (int)Mathf.Max(currentHealth,healthSlider.minValue);//Mathf.Max為浮點數
+    }
+
+    protected void Update()
+    {
+        healthSlider.value = Mathf.Lerp(healthSlider.value, currentHealth, Time.deltaTime * speed);
+    }
+}
